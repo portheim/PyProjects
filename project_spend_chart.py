e175d978d5e1b3c664fe1a7f505cd3f271691792
portheim <homsik.portheim@proton.me>
2026-03-28 23:38:07 +0900

    Fixing the chart.

--- project_spend_chart.py
+++ project_spend_chart.py
@@ -90,19 +90,28 @@ def create_spend_chart(categories):
     #Creates the circles for each category.
     count = 0
     for key, value in breakdown.items():
-        for i in range(1, 11):
+        for i in range(10, 0, -1):
             if i <= value:
                 if count == 0:
-                    line[12 - i] += " o"
+                    line[11 - i] += " o"
                 else:
-                    line[12 - i] += "  o"
+                    line[11 - i] += "  o"
             else:
                 if count == 0:
-                    line[12 - i] += "  "
+                    line[11 - i] += "  "
                 else:
-                    line[12 - i] += "   "
+                    line[11 - i] += "   "
+        
+        for j in range(len(breakdown.items())):
+            if count == 0:
+                line[11] += " o"
+            else:
+                line[11] += "  o"
+
         count += 1
-    
+        
+
+
     return '\n'.join(line[:13])
             
 
