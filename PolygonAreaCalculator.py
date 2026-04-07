890851740d2b59e94c2f45c69e7abe6fa9943b8b
portheim <homsik.portheim@proton.me>
2026-04-07 10:12:17 +0900

    Refactor PolygonAreaCalculator.py for clarity

--- PolygonAreaCalculator.py
+++ PolygonAreaCalculator.py
@@ -67,23 +67,3 @@ class Square(Rectangle):
                 picture += '*'
             picture += '*\n'
         return picture
-
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
-
-rect.set_height(10)
-rect.set_width(15)
-sq.set_width(5)
-print(rect.get_amount_inside(sq))
-print(Rectangle(4,8).get_amount_inside(Rectangle(3, 6)))
