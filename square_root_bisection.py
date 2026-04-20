f93840f5e68fd42022f2152d77752c2b8369f6f4
portheim <homsik.portheim@proton.me>
2026-04-20 23:29:59 +0900

    Added main method and user input selection for square root calculation method.

--- square_root_bisection.py
+++ square_root_bisection.py
@@ -5,8 +5,8 @@ def square_root_bisection(value, tolerance=0.1, iterations=10):
     if value > 4:
         high = value / 2
     else:
-        high = max(1, value)    
-    
+    high = max(1, value)    
+
     low, result = 0, 0
 
     if value * value == value:  #Checks for 0 or 1
@@ -19,9 +19,10 @@ def square_root_bisection(value, tolerance=0.1, iterations=10):
         if its == iterations:
             print(f'Failed to converge within {iterations} iterations')
             return None
-        
+
         mid = (low + high) / 2
         sq_mid = mid * mid
+
         if sq_mid == value:
             result = mid
             break
@@ -32,5 +33,7 @@ def square_root_bisection(value, tolerance=0.1, iterations=10):
             high = mid 
         its += 1
 
-    print(f'The square root of {value} is approximately {result}')
+    print(f'The square root of {value} is approximately {result}. Done in {its} iterations.')
     return result
+
+square_root_bisection(1000, 0.1, 20)
